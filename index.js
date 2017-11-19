89b64276f4ee724f1cf80ef94e5d0092feecec9c
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2017-11-18 22:43:22 -0800

    Update code style.

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ var path = require('path');
 var wordlist = {};
 
 ['english', 'american', 'british', 'canadian'].forEach(function (dialect) {
-    var dialectKey = dialect === 'english' ? dialect : 'english/' + dialect;
-    var dialectWords = [];
-    [10, 20, 35, 40, 50, 55, 60, 70].forEach(function (frequency) {
-        var frequencyKey = dialectKey + '/' + frequency;
-        var frequencyWords = require(path.join(__dirname, dialect + '-words-' + frequency + '.json'));
-        wordlist[frequencyKey] = frequencyWords;
-        Array.prototype.push.apply(dialectWords, frequencyWords);
-    });
-    wordlist[dialectKey] = dialectWords.sort();
+  var dialectKey = dialect === 'english' ? dialect : 'english/' + dialect;
+  var dialectWords = [];
+  [10, 20, 35, 40, 50, 55, 60, 70].forEach(function (frequency) {
+    var frequencyKey = dialectKey + '/' + frequency;
+    var frequencyWords = require(path.join(__dirname, dialect + '-words-' + frequency + '.json'));
+    wordlist[frequencyKey] = frequencyWords;
+    dialectWords.push(...frequencyWords);
+  });
+  wordlist[dialectKey] = dialectWords.sort();
 });
 
 module.exports = wordlist;
--- scripts/generate.js
+++ scripts/generate.js
@@ -3,31 +3,28 @@
 var fs = require('fs');
 var path = require('path');
 
-function splitWordlist(contents) {
-    return contents.trim().split('\n');
+function splitWordlist (contents) {
+  return contents.trim().split('\n');
 }
 
-function removePossesives(words) {
-    return words.filter(function (word) {
-        return !/'s$/.test(word);
-    });
+function removePossesives (words) {
+  return words.filter(function (word) {
+    return !/'s$/.test(word);
+  });
 }
 
-function processWordlist(wordlist) {
-    return removePossesives(splitWordlist(wordlist)).sort();
+function processWordlist (wordlist) {
+  return removePossesives(splitWordlist(wordlist)).sort();
 }
 
-function getWordlist(name, frequency) {
-    var filePath = path.join(__dirname, '..', 'sources', name + '.' + frequency);
-    return processWordlist(fs.readFileSync(filePath, 'utf8'));
+function getWordlist (name, frequency) {
+  var filePath = path.join(__dirname, '..', 'sources', name + '.' + frequency);
+  return processWordlist(fs.readFileSync(filePath, 'utf8'));
 }
 
-var nationalities = ['english', 'american', 'british', 'canadian'];
-var frequencies = [10, 20, 35, 40, 50, 55, 60, 70];
-
-nationalities.forEach(function (nationality) {
-    frequencies.forEach(function (frequency) {
-        var filePath = path.join(__dirname, '..', nationality + '-words-' + frequency + '.json');
-        fs.writeFileSync(filePath, JSON.stringify(getWordlist(nationality + '-words', frequency)));
-    });
+['english', 'american', 'british', 'canadian'].forEach(function (nationality) {
+  [10, 20, 35, 40, 50, 55, 60, 70].forEach(function (frequency) {
+    var filePath = path.join(__dirname, '..', nationality + '-words-' + frequency + '.json');
+    fs.writeFileSync(filePath, JSON.stringify(getWordlist(nationality + '-words', frequency)));
+  });
 });
